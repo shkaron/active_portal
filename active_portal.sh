01fd9f0f987092a0c12a030ff70da844d00cab94
shkaron <64483722+shkaron@users.noreply.github.com>
2021-08-27 07:31:32 +0000

    Update active_portal.sh

--- active_portal.sh
+++ active_portal.sh
@@ -1,4 +1,3 @@
-  GNU nano 5.8                                                                 iptables_usage.sh                                                                            
 #!/bin/bash
 
 
